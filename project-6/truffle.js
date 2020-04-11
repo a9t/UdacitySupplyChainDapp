f6e5257ecef350584a9c104bad406baf9eb30f3e
a9t <tigora.andrei@gmail.com>
2020-04-11 14:24:15 +0200

    feat: Add Rinkeby network config

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,21 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const fs = require('fs');
+
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = fs.readFileSync(".infura_key").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 5500000,
+    },
   }
 };
